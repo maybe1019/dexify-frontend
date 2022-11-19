af22358e30d0bcc73026a5421415dce7d7f48f6e
S.Tanaka <stenor0515@gmail.com>
2022-11-19 14:52:00 -0700

    fix portfolio css and added split text

--- src/pages/Portfolio/components/DexfundSplit.tsx
+++ src/pages/Portfolio/components/DexfundSplit.tsx
@@ -9,39 +9,44 @@ function DexfundSplit() {
       <div className=" text-text-1 dark:text-text-1-dark text-xl">
         Dexfund Split
       </div>
-      <div className="flex">
-        <PieChart width={270} height={270}>
-          <Pie
-            stroke="none"
-            cx="50%"
-            cy="50%"
-            legendType="circle"
-            data={fundUsersData}
-            nameKey="name"
-            dataKey="value"
-            startAngle={180}
-            endAngle={-180}
-            cornerRadius={100}
-            innerRadius={60}
-            outerRadius={80}
-            paddingAngle={-10}
-            labelLine={true}
-            label
-            isAnimationActive={true}
-          >
-            {fundUsersData.map((entry, index) => (
-              <Cell key={`slice-${index}`} fill={colorArray[index]} />
-            ))}
-          </Pie>
-        </PieChart>
+      <div className="flex justify-between">
+        <div className="relative text-center">
+          <PieChart width={270} height={270}>
+            <Pie
+              stroke="none"
+              cx="50%"
+              cy="50%"
+              legendType="circle"
+              data={fundUsersData}
+              nameKey="name"
+              dataKey="value"
+              startAngle={180}
+              endAngle={-180}
+              cornerRadius={100}
+              innerRadius={60}
+              outerRadius={80}
+              paddingAngle={-10}
+              labelLine={true}
+              label
+              isAnimationActive={true}
+            >
+              {fundUsersData.map((entry, index) => (
+                <Cell key={`slice-${index}`} fill={colorArray[index]} />
+              ))}
+            </Pie>
+          </PieChart>
+          <div className="absolute top-0 left-0 text-xl text-primary w-full h-full flex items-center justify-center">
+            Split
+          </div>
+        </div>
         <div className="flex flex-col justify-around">
           {fundUsersData.map((entry, index) => (
-            <div className="flex">
+            <div className="flex items-center">
               <div
                 className="w-3 h-3 rounded-full mr-3"
                 style={{ backgroundColor: colorArray[index] }}
               ></div>
-              <span className="text-xs">{entry.name}</span>
+              <div className="text-xs">{entry.name}</div>
             </div>
           ))}
         </div>
